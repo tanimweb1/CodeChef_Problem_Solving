413ea2e62474cde623edf762526099fb9897adfa
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-16 22:29:19 +0600

    Add files via upload

---
+++ Fixed_Points.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+ while (T--)
+    {
+        int a, b;
+        cin>>a>>b;
+
+        if (a-b == 1)
+        {
+            cout<<"No"<<endl;
+        }
+        else
+        {
+            cout <<"Yes"<<endl;
+        }
+    }
+
+    return 0;
+}
