2be380b6ae8e241cfd3a4a81e6ecdfb741fb7b6c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-18 23:44:53 +0600

    Add files via upload

---
+++ Masterchef_finals.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+int T;
+cin>>T;
+while(T--){
+
+int x;
+cin>>x;
+
+if(x<=10 && x>=1){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;;
+}
+
+
+
+
+}
+
+
+
+
+
+
+    return 0;
+}
