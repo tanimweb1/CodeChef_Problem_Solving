f647b38688c3cf95fd107bc718c2f1a87beb918e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-18 11:50:51 +0600

    Add files via upload

---
+++ Saving_Taxes.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+
+while(T--){
+int x,y;
+cin>>x>>y;
+
+int invest = x-y;
+cout<<invest<<endl;
+
+
+
+}
+
+
+
+    return 0;
+}
