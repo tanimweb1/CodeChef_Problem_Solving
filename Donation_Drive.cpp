2ae4d452b60b5c87d227511485f12e6f1be9aa81
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 00:05:49 +0600

    Add files via upload

---
+++ Donation_Drive.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+while(T--){
+
+int x,y;
+cin>>x>>y;
+int need = x-y;
+cout<<need<<endl;
+
+
+}
+
+
+
+
+
+    return 0;
+}
