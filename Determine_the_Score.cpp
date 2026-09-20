8891b93744a2922f12ae6af3e6c1f2d5aaf2aa9c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-20 11:16:00 +0600

    Add files via upload

---
+++ Determine_the_Score.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+while(T--){
+int x,n;
+cin>>x>>n;
+int ans = x/10;
+
+int result = ans*n;
+
+cout<<result<<endl;
+
+}
+
+
+
+
+
+    return 0;
+}
