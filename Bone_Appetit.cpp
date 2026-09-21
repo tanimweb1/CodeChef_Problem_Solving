9dcfc05b9eedf37ca8a3cfac4bb7087263505be9
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 16:18:02 +0600

    Add files via upload

---
+++ Bone_Appetit.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int b,blo;
+cin>>b>>blo;
+
+int bt,bloT;
+cin>>bt>>bloT;
+
+int total_bon = b*bt;
+int total_blo = blo*bloT;
+int sum = total_blo + total_bon;
+cout<<sum<<endl;
+
+
+
+
+    return 0;
+}
