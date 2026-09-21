522be8c454848ac99b83f5c118c8c056e57fe2c0
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 16:29:45 +0600

    Add files via upload

---
+++ Reach_the_Target.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+int t;
+cin>>t;
+while(t--){
+int x,y;
+cin>>x>>y;
+
+int need = x-y;
+cout<<need<<endl;
+
+}
+
+
+
+
+    return 0;
+}
