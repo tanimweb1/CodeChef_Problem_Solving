59ae72acc4b3b84f76ff47bfb245fc687115513e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 16:13:51 +0600

    Add files via upload

---
+++ Puzzle_Hunt.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+
+
+int s;
+cin>>s;
+if(s>=6 && s<=8){
+    
+    cout<<"Yes"<<endl;
+}
+else {
+    cout<<"No"<<endl;
+}
+
+
+
+
+
+    return 0;
+}
