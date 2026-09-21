0ca4276335218993a28098ce21b5c390a380ecd3
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 16:07:46 +0600

    Add files via upload

---
+++ Audible_Range.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+while(T--){
+
+
+int s;
+cin>>s;
+if(s>=67 && s<=45000){
+
+    cout<<"YES"<<endl;
+}
+
+else {
+    cout<<"NO"<<endl;
+}
+}
+
+
+
+
+    return 0;
+}
