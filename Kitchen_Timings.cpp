b055d8a7a6fc049b933266e384ff518025480abb
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 15:57:04 +0600

    Add files via upload

---
+++ Kitchen_Timings.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+while(T--){
+
+
+int x,y;
+cin>>x>>y;
+int ans = y-x;
+cout<<ans <<endl;
+
+}
+
+
+
+
+    return 0;
+}
