7261407fec957b84614159339150628b52d16bb2
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-21 16:11:03 +0600

    Add files via upload

---
+++ Reach_on_Time.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int T;
+cin>>T;
+while(T--){
+
+
+int s;
+cin>>s;
+if(s>=30){
+
+    cout<<"YES"<<endl;
+}
+
+else {
+    cout<<"NO"<<endl;
+}
+}
+
+
+
+
+    return 0;
+}
