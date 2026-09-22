b81a3a3d8fd9563f14c370257ac50cbd5e7000dd
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-22 18:42:41 +0600

    Add files via upload

---
+++ Subscriptions.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+int n,m;
+cin>>n>>m;
+
+      int result = ceil(n / 6.0);
+
+        cout << result * m << endl;
+
+}
+
+
+
+
+
+    return 0;
+}
