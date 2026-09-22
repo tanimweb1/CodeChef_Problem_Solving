b28e6ebbade5c60fe4efaf1ec7e7bc819e138302
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-22 18:16:24 +0600

    Add files via upload

---
+++ Greater_Average.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+double a,b,c;
+cin>>a>>b>>c;
+
+double avg = (a+b)/2.0;
+
+
+if(avg>c){
+    cout<<"YES"<<endl;
+}
+else {
+    cout<<"NO"<<endl;
+}
+
+
+}
+
+
+
+
+
+    return 0;
+}
