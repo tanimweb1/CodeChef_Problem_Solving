7e07ff7fb9c43f427ce9d1aba3d7868cf520bc36
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-24 18:02:24 +0600

    Add files via upload

---
+++ Chef_in_his_Office.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+int x,y;
+cin>>x>>y;
+
+int nday = x*4;
+ int total = nday + y;
+cout<<total<<endl;
+
+}
+
+
+
+
+
+
+    return 0;
+}
