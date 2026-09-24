92385aa17e12e5f4df4a6c072d9051989dc64f9f
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-24 17:27:02 +0600

    Add files via upload

---
+++ Janmansh_and_Assignments.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+int x;
+cin>>x;
+if(x<=7){
+    cout<<"Yes"<<endl;
+}
+else{
+    cout<<"No"<<endl;
+}
+
+
+
+
+}
+
+
+
+
+
+
+    return 0;
+}
